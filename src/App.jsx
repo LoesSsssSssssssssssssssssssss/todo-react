792e9ebd466e26d83ac1d247a348fcf5b2ce5e20
Loes_s <103633598+LoesSsssSssssssssssssssssssss@users.noreply.github.com>
2024-01-21 14:24:58 +0300

    part three
    post and delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,75 @@
-import './App.css'
-import Task from './Task'
-import { useEffect, useState } from "react";
+import './App.css';
+import Task from './Task';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
 function App() {
-
   const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState('');
+
+  const deleteTask = (todo) => {
+    axios
+      .delete(`http://localhost:3000/api/todos/${todo.id}`)
+      .then((response) => {
+        updateData();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   const addTodo = () => {
-    if (todo !== "") {
-      setTodos([...todos, todo]);
-      setTodo("");
+    // if (todo !== '') {
+    //   setTodos([...todos, todo]);
+    //   setTodo('');
+    // }
+
+    if (todo !== '') {
+      axios
+        .post('http://localhost:3000/api/todos', {
+          name: todo,
+          isDone: 'false',
+        })
+        .then((response) => {
+          setTodos([...todos, response.data]);
+          setTodo('');
+        })
+        .catch((error) => {
+          console.log(error, 'error');
+        });
     }
-  }
+  };
+
+  const updateData = () => {
+    axios
+      .get('http://localhost:3000/api/todos')
+      .then((data) => {
+        setTodos(data.data);
+      })
+      .catch(setTodos([]));
+  };
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/todos').then(data => {
-      // console.log(data.data, "data")
-      setTodos(data.data)
-    }).catch(e => console.log(e, "error"))
-  }, [])
+    axios
+      .get('http://localhost:3000/api/todos')
+      .then((data) => {
+        //console.log(data.data, 'data');
+        setTodos(data.data);
+      })
+      .catch((e) => console.log(e, 'error'));
+  }, []);
 
   useEffect(() => {
-    console.log(todos, "todos")
-  }, [todos])
+    console.log(todos, 'todos');
+  }, [todos]);
 
   return (
     <div className="wrapper">
       <div className="bg-white shadow sm:rounded-lg w-6/12">
         <div className="px-4 py-5 sm:p-6">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">Введите задачу</h3>
+          <h3 className="text-base font-semibold leading-6 text-gray-900">
+            Введите задачу
+          </h3>
           <div className="mt-5 sm:flex sm:items-center">
             <div className="w-full sm:max-w-xs">
               <input
@@ -42,7 +80,7 @@ function App() {
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 padding pl-5"
                 placeholder="Введите задачу"
                 onChange={(e) => {
-                  setTodo(e.target.value)
+                  setTodo(e.target.value);
                 }}
               />
             </div>
@@ -55,9 +93,9 @@ function App() {
           </div>
         </div>
       </div>
-      <Task todos={todos} />
+      <Task todos={todos} deleteTask={deleteTask} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
